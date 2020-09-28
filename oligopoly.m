6635a0741dc532966e41b9b769455e3a34e4a1bc
unknown <nian15527005643@163.com>
2020-09-28 15:57:47 +0800

    修一下bug，可以进行基本运算

--- oligopoly.m
+++ oligopoly.m
@@ -2,13 +2,12 @@ clc;clear;
 N=10; %定义药品种类数目
 M=5;                     %定义电子零售商数目,取实体为M+1
 gamma=rand(N,1);          %定义其他参数,gamma为药物属性向量  
-qulity=rand(N,1);   %药品质量
-lamuta=ones(M+1,1);  %药品商属性
+qulity=rand(N,1);        %药品质量
+lamuta=ones(M+1,1);      %药品商属性
 %kexi=ones(N,M+1);
 w=0.7+0.1*ones(N,M+1);  %药品规模价格
 price=ones(N,M+1);
-xita=zeros(M+1,1);  %报销比例
-xita(M+1)=0.5;
+xita=1;  %报销比例
 beta=1;
 alpha=1;
 tao=0.8;
@@ -16,22 +15,22 @@ for h=1:100
 miu=0.01*h;
 for i=1:N
     for j=1:M+1
-        a(i,j)=(lamuta(j)+gamma(i)+alpha*qulity(i))/miu;         %a和b赋值区域
+        a(i,j)=(lamuta(j)+gamma(i)+alpha*qulity(i))/miu;         %a为商品i在零售商j中得质量属性
     end
 end
-for i=1:M+1
-b(i)=beta*(1-xita(i))/miu;
-end
+
+b=beta*(1-xita)/miu;       %b为消费者价格敏感度
+
 temp=0;
 for i=1:N
-    temp=temp+exp(a(i,M+1)-b(1)*price(i,M+1));
+    temp=temp+exp(a(i,M+1)-b(1)*price(i,M+1));          
 end
-temp2=0;
 for j=1:M         %给A赋值，零售商调整过的吸引力
+    temp2 = 0
     for i=1:N
-        temp2=temp2+exp(a(i,j)-b*w(i,j));
+        temp2 = temp2 + exp(a(i,j)-b*w(i,j));
     end
-    A_bar(j)=exp(-1)*(temp^-1*temp2)^miu;
+    A_bar(j)=exp(-1)*(temp^(-1)*temp2)^miu;
 end
 fun=@(t)t+V(t*A_bar(1))+V(t*A_bar(2))+V(t*A_bar(3))+V(t*A_bar(4))+V(t*A_bar(5))-1
 d0=fzero(fun,0.5)
